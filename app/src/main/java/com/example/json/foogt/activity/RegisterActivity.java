140682ccda90b85b933707f266c8d6dd547ce3df
eric <ericceshi1@163.com>
2016-01-08 14:04:01 +0800

    UI Fix

--- app/src/main/java/com/example/json/foogt/activity/RegisterActivity.java
+++ app/src/main/java/com/example/json/foogt/activity/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
@@ -48,10 +49,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
         bar = getSupportActionBar();
 
-        if(bar!=null){
+        if (bar != null) {
             bar.setDisplayHomeAsUpEnabled(true);
             bar.setDisplayShowHomeEnabled(true);
             bar.setHomeButtonEnabled(true);
